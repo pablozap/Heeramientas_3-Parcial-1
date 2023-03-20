c54c642e662d7229a87e48d012b6f01d9b1afb40
David Ramirez Usuga <dramirez9830@gmail.com>
2023-03-20 13:26:42 -0500

    Backend terminado

--- accion.php
+++ accion.php
@@ -4,40 +4,80 @@
     switch($_POST["opc"]){
         case 1:
             echo "INICIO SESION...<br>";
-            $ban = false;
+            $ban_usuario = false;
+            $ban_contrasena = false;
             $archivo = fopen("usuarios.txt", "r");
             while(!feof($archivo)){
                 $linea = fgets($archivo);
                 if($linea != null){
                     $info = explode(";", $linea);
                     $persona = new Persona($info[0], $info[1], $info[2]);
-                    if( $_POST["user"] == $persona->getUser() && $_POST["pass"] == $persona->getPassword()){
+                    if( $_POST["user"] == $persona->getUser()  ){
 
-                        $ban = true;
-                    }
+                        $ban_usuario = true;
+
+                        if ($_POST["pass"] == $persona->getPassword()){
+                           
+                            $ban_contrasena = true;
+                            break;
+                            
+                        }
+                     
+                    } 
                 }
             }
             fclose($archivo);
-            if($ban == true){
-                echo "Inicio correctamente :)<br>";
-            }else{
-                echo "NO inicio correctamente :(<br>";
+
+            if($ban_usuario == false){
+                echo "El usuario no existe :(<br>";
+            }elseif($ban_contrasena== false){
+                echo "La contraseña es incorrecta  :(<br>";
+            } else {
+                echo "Inicio sesión correctamente  :)<br>";
             }
             echo "<br><a href='index.html'>Ir al inicio de sesión...</a>";
             break;
         case 2:
             echo "REGISTRO...";
+            
             $persona = new Persona(trim($_POST["id"]), trim($_POST["user"]), trim($_POST["pass"]));
             $vacio = (!empty($persona->getId()) && !empty($persona->getUser()) && !empty($persona->getPassword()));
-            echo $vacio;
-            print_r($persona);
-            if ($vacio){
+            if($vacio) {
+                $existe_usuario = false;
+                $usuarios = fopen ("usuarios.txt","r");
+                while (!feof($usuarios)) {
+                    $linea = fgets($usuarios);
+                    if ($linea != null){
+                    $datos_usuario = explode(";", $linea);
+                    $dato_nuevo = new Persona($datos_usuario[0],$datos_usuario[1], $datos_usuario[2]);
+                        if ($dato_nuevo->getUser() == $persona->getUser()) {
+                        $existe_usuario = true;
+                  break;
+                }
+             }
+
+             
+            }
+
+            fclose($usuarios);
+
+            
+            echo ($existe_usuario);
+         
+            if ($existe_usuario == true){
+                echo "El usuario ya existe ";
+            } else {
                 $archivo = fopen("usuarios.txt", "a");
                 $usuarioRegistro = $persona->getId() . $delimitador . $persona->getUser(). $delimitador . $persona->getPassword();
                 fwrite($archivo, $usuarioRegistro.PHP_EOL);
                 fclose($archivo);
+                echo "El usuario fue registrado existosamente";
             }
-            echo "<br><a href='registro.html'>Ir al registro...</a>";
+           
+            } else {
+                echo "Solo se permiten letras y números, sin espacios";
+            }
+           
             break;
     }
 ?>
